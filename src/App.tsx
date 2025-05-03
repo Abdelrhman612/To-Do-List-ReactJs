ac18ad059f0dad3acb0cb9b725a0d8b9a25429be
Abdelrhman <abdo612200320@gmail.com>
2025-05-03 16:16:29 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Todoform from "./components/Todoform/Todoform";
+import Todo from "./components/Todo/Todo";
 
 function App() {
-  const [todos, setTodos] = useState<string[]>([]);
-  const AddTodo = (todo: string) => {
+  const [todos, setTodos] = useState<{ id: string; text: string }[]>([]);
+  const AddTodo = (todo: { id: string; text: string; complete: boolean }) => {
     setTodos([todo, ...todos]);
   };
   return (
     <>
       <div className="container">
-        <Todoform onSubmit={AddTodo} />
+        <Todoform onSubmit={AddTodo} todos={[]} />
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
       </div>
     </>
   );
