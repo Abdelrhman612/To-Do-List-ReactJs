6b56cc36a7643533ad3d3300eef00c0cc13fc57a
Abdelrhman <abdo612200320@gmail.com>
2025-05-04 03:45:33 +0300

    update Todo.tsx

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -3,7 +3,15 @@ import "./TodoStyle.css";
 const Todo = (props: TodoProps) => {
   return (
     <div className="d-f">
-      <div className="text">{props.todo.text}</div>
+      <div
+        style={{
+          textDecoration: props.todo.complete ? "line-through" : "none",
+        }}
+        onClick={props.toggleComplete}
+        className="text"
+      >
+        {props.todo.text}
+      </div>
       <button className="delete-btn" onClick={props.onDelete}>
         x
       </button>
